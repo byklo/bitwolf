d0270925edf6e69e7945f1c9e04a96cd6c0ed8e9
Bryan Lo <byklo@uwaterloo.ca>
2017-09-12 02:18:57 -0400

    moved price stream logic into its own actor

--- candles/candles.scala
+++ candles/candles.scala
@@ -1,4 +1,4 @@
-import akka.actor.{ActorSystem, Actor, ActorLogging, Props}
+import akka.actor.{ActorSystem, Actor, ActorRef, ActorLogging, Props}
 import akka.{Done, NotUsed}
 import akka.http.scaladsl.Http
 import akka.stream.ActorMaterializer
@@ -23,49 +23,60 @@ class CandleBuilder extends Actor with ActorLogging {
 }
 
 
-object App {
+object PriceStream {
+  def props(subscriber: ActorRef): Props = Props(new PriceStream(subscriber))
+}
+
+class PriceStream(subscriber: ActorRef) extends Actor {
+  import scala.concurrent.ExecutionContext.Implicits.global
+
+  implicit val materializer = ActorMaterializer()
+
+  val subscriptionPayload = """
+    {
+      "event": "subscribe",
+      "channel": "trades",
+      "symbol": "BTCUSD"
+    }
+  """
+
+  val handleIncoming: Sink[Message, Future[Done]] = Sink.foreach {
+    case message: TextMessage.Strict =>
+      val json = Json.parse(message.text)
+      val messageType = (json \ 1).asOpt[String]
+      if (messageType.contains("te")) {
+        subscriber ! (json \ 4).as[Double]
+      }
+    case _ =>
+  }
+
+  val websocketSubscription: Source[Message, NotUsed] = Source.single(TextMessage(subscriptionPayload)).concatMat(Source.maybe[Message])(Keep.left)
+  val flow: Flow[Message, Message, Future[Done]] = Flow.fromSinkAndSourceMat(handleIncoming, websocketSubscription)(Keep.left)
+  val (upgradedResponse, closed) = Http()(context.system).singleWebSocketRequest(WebSocketRequest("wss://api.bitfinex.com/ws"), flow)
+
+  val connected = upgradedResponse.map { upgrade =>
+    if (upgrade.response.status == StatusCodes.SwitchingProtocols) {
+      Done
+    } else {
+      throw new RuntimeException(s"Connection failed: ${upgrade.response.status}")
+    }
+  }
+
+  connected.onComplete(println)
+  closed.foreach(_ => println("closed"))
+
+  def receive: Receive = {
+    case _ =>
+  }
+}
+
+
+object CandlesApp {
   def main(args: Array[String]) {
     implicit val system = ActorSystem()
-    implicit val materializer = ActorMaterializer()
     import system.dispatcher
 
-    val candleman = system.actorOf(CandleBuilder.props(), "candleman")
-
-    val printSink: Sink[Message, Future[Done]] = Sink.foreach {
-      case message: TextMessage.Strict =>
-        val json = Json.parse(message.text)
-        val messageType = (json \ 1).asOpt[String]
-        if (messageType.contains("te")) {
-          candleman ! (json \ 4).as[Double]
-        }
-      case _ =>
-    }
-
-    val subscriptionPayload = """
-      {
-        "event": "subscribe",
-        "channel": "trades",
-        "symbol": "BTCUSD"
-      }
-    """
-
-    val subscribeTrades: Source[Message, NotUsed] = Source.single(TextMessage(subscriptionPayload)).concatMat(Source.maybe[Message])(Keep.left)
-
-    val flow: Flow[Message, Message, Future[Done]] = Flow.fromSinkAndSourceMat(printSink, subscribeTrades)(Keep.left)
-
-    val (upgradedResponse, closed) = Http().singleWebSocketRequest(WebSocketRequest("wss://api.bitfinex.com/ws"), flow)
-
-    val connected = upgradedResponse.map { upgrade =>
-      if (upgrade.response.status == StatusCodes.SwitchingProtocols) {
-        Done
-      } else {
-        throw new RuntimeException(s"Connection failed: ${upgrade.response.status}")
-      }
-    }
-
-    connected.onComplete(println)
-    closed.foreach(_ => println("closed"))
-
-    println("ok")
+    val candles = system.actorOf(CandleBuilder.props(), "candles")
+    val priceStream = system.actorOf(PriceStream.props(candles), "priceStream")
   }
 }
